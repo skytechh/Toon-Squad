44f9b05e5032f352c709b04cb5086fedb1786169
skytechh <skytechh@outook.com>
2015-05-21 16:19:35 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+/**
+ * CS 141: Introduction to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Assignment #N
+ *
+ * <description-of-assignment>
+ *
+ * Team #N / (or name if individual)
+ *   <team-member-names-if-team-assignment>
+ */
+package edu.cpp.cs.cs141.teamproject;
+
+/**
+ * @author Toon Squad
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		UI ui = new UI();
+	}
+
+}
