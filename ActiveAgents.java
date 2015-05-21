9342bb35ae031d5f8a79c128a1595c162de4c18c
skytechh <skytechh@outook.com>
2015-05-21 16:20:33 -0700

    Create ActiveAgents.java

---
+++ ActiveAgents.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.teamproject;
+
+/**
+ * @author Toon Squad
+ *
+ */
+public class ActiveAgents {
+
+	int hp;
+
+	public ActiveAgents() {
+		hp = 1;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+}
